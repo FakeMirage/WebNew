383113dd1a64e97b85957682a917e5d8a44bf929
unknown <lucasmbsd@gmail.com>
2018-11-22 17:09:38 -0200

    mudança nos script

---
+++ Scripts/js/myJS.js
@@ -0,0 +1,20 @@
+function changeSkills(idRecebida)
+{
+	if(idRecebida =="anao")
+	{
+		document.getElementById("bolaDeFogo").value = "kk";
+				
+		document.getElementById("iPowers2").value = "new HTML";
+
+		document.getElementById("iPowers3").value = "new HTML";
+
+		document.getElementById("iPowers4").value = "new HTML";
+
+		document.getElementById("iPowers5").value = "new HTML";
+
+	}
+	else if (document.getElementById().selectedIndex == 1)
+	{
+
+	}
+}
